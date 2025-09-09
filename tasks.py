3f28b10a911cfcaa443e0106e597d143a6cb3bee
Prateek Dalmia <dalmia01@gmail.com>
2025-09-09 17:54:23 +0530

    Update tasks.py
    added a function to remove duplicates in an array and append a placeholder

--- tasks.py
+++ tasks.py
@@ -19,3 +19,30 @@ print("Total jewels that are stones : ", findJewelStones("z", "ZZ"))
 print("Total jewels that are stones : ", findJewelStones(1, 123))
 print("Total jewels that are stones : ", findJewelStones(". ", "AA "))
 print("Total jewels that are stones : ", findJewelStones("z ZZ", "ZZ Cr"))
+
+print("----------")
+
+def removeDuplicatesWithTrailingPlaceholder(arr, placeholder = "*"):
+    """
+    return an array which only contains uniques in sorted array
+    """
+    if (not arr) or len(arr) == 0:
+        return []
+        
+    newArr = [arr[0]]
+    count = 0
+    for index in range(1, len(arr)):
+        if arr[index] in newArr:
+            count += 1
+        else:
+            newArr.append(arr[index])
+    
+    newArr.extend(placeholder * count)
+            
+    return newArr
+        
+print("Remove duplicates array with trailing placeholders : ", removeDuplicatesWithTrailingPlaceholder([0,1,1,1,1,2,2,2,3,3,4]))
+print("Remove duplicates array with trailing placeholders : ", removeDuplicatesWithTrailingPlaceholder([]))
+print("Remove duplicates array with trailing placeholders : ", removeDuplicatesWithTrailingPlaceholder([1], "_"))
+print("Remove duplicates array with trailing placeholders : ", removeDuplicatesWithTrailingPlaceholder([1, 1, 1], "+"))
+print("Remove duplicates array with trailing placeholders : ", removeDuplicatesWithTrailingPlaceholder([1, 1, 2, 2, 3, 4, 4], "%"))
